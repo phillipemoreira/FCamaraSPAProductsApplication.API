70dda373cf8a05d9828683ec63ed37a4e1feab6e
phillipeamoreira <phillipealexmoreira@gmail.com>
2016-11-26 01:24:21 -0200

    Adding API project

---
+++ FCamaraSPAProductsApplication.API/Models/ProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace FCamaraSPAProductsApplication.API
+{
+    public class ProductModel
+    {
+        [Key]
+        public string ProdId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class ProductContext : DbContext
+    {
+        public DbSet<ProductModel> Product { get; set; }
+    }
+}
