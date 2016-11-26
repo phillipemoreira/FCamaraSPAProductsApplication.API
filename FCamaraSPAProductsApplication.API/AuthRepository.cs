40d410732931dc430a5bf941367e835887c970c5
phillipeamoreira <phillipealexmoreira@gmail.com>
2016-11-26 01:51:33 -0200

    Adding API

---
+++ FCamaraSPAProductsApplication.API/AuthRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Threading.Tasks;
+
+namespace FCamaraSPAProductsApplication.API
+{
+    public class AuthRepository : IDisposable
+    {
+        private AuthContext _context;
+
+        private UserManager<IdentityUser> _userManager;
+
+        public AuthRepository()
+        {
+            _context = new AuthContext();
+            _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_context));
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserModel userModel)
+        {
+            IdentityUser user = new IdentityUser
+            {
+                UserName = userModel.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, userModel.Password);
+
+            return result;
+        }
+
+        public async Task<IdentityUser> FindUser(string userName, string password)
+        {
+            IdentityUser user = await _userManager.FindAsync(userName, password);
+
+            return user;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            _userManager.Dispose();
+
+        }
+    }
+}
---
+++ FCamaraSPAProductsApplication.API/Controllers/ProducController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace FCamaraSPAProductsApplication.API.Controllers
+{
+    [RoutePrefix("api/Products")]
+    public class ProductController : ApiController
+    {
+        private ProductContext db = new ProductContext();
+
+        // GET: api/Products
+        [Authorize]
+        [Route("")]
+        public IQueryable<ProductModel> GetProduct()
+        {
+            return db.Product;
+        }
+
+        // GET: api/Products/5
+        [Authorize]
+        [Route("")]
+        [ResponseType(typeof(ProductModel))]
+        public IHttpActionResult GetProductModel(string id)
+        {
+            ProductModel productModel = db.Product.Find(id);
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(productModel);
+        }
+    }
+}
